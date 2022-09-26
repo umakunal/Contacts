7de22a071c67498be9e9ccf19ea396dc3d5b3387
Kunal Poddar <kunal.poddar@codepoetry.in>
2022-09-26 07:07:11 +0530

    Drawer Navigator added

---
+++ src/navigation/DrawerNavigator.js
@@ -0,0 +1,18 @@
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NAVIGATION} from '../constants/navigation';
+import UserNavigator from './UserNavigator';
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name={NAVIGATION.homeNavigator}
+        component={UserNavigator}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export default DrawerNavigator;
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,12 +2,14 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthNavigator from './AuthNavigator';
 import UserNavigator from './UserNavigator';
+import DrawerNavigator from './DrawerNavigator';
 
 const MainNavigator = () => {
   return (
     <NavigationContainer>
       {/* <AuthNavigator /> */}
-      <UserNavigator />
+      {/* <UserNavigator /> */}
+      <DrawerNavigator />
     </NavigationContainer>
   );
 };
